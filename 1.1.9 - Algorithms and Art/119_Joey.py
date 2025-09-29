3d582c5aa8d28c4726927eaca44e044d147347c7
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-09-29 11:06:45 -0500

    Update 119_Joey.py
    updated framework of how to switch seasons

--- 1.1.9 - Algorithms and Art/119_Joey.py
+++ 1.1.9 - Algorithms and Art/119_Joey.py
@@ -1,46 +1,61 @@
 # importing modules
 import turtle as trtl
 import random as rdm
-import keyboard as key
-import time as time
+import time as tme
 painter = trtl.Turtle()
 # screen and variables
 screen_h = 1000
 screen_w = 1500
 wn = trtl.Screen()
 wn.setup(width=screen_w, height=screen_h)
-start = ("c")
+start = ("idk")
 season = ("idk")
+creating = 1
+def createpainter():
+    painter.pensize(10)
+    painter.pencolor("black")
 
 def changeseason():
     season = trtl.textinput("Confirmation:","What season would you like? Summer, Fall, Winter, Spring")
-trtl.onkeypress(changeseason, "c")
-
+wn.onkeypress(changeseason, "c")
+wn.listen()
 start = trtl.textinput("Confirmation:","Press C to bring up the menu to set the season! Enter 'y' to continue!")
-wn.onkeypress(changeseason, "c") 
+
 
 while start != ("y"):
     start = trtl.textinput("Confirmation:","Press C to bring up the menu to set the season! Enter 'y' to continue!")
 
 
 while start == ("y"):
-    season = trtl.textinput("Confirmation:","What season would you like? Summer, Fall, Winter, Spring")
+    changeseason()
     if season == ("Summer"):
-        print("hi")
+        if creating == 1:
+            createpainter()
+            painter.forward(100)
+            painter.forward(100)
+            painter.forward(100)
+            painter.forward(100)
+            painter.forward(100)
+            painter.forward(100)
+            creating - 1
+
     
-    if season == ("Fall"):
-        print("hi")
+    while season == ("Fall"):
+        createpainter()
+        painter.forward(100)
     
-    if season == ("Winter"):
-        print("hi")
+    while season == ("Winter"):
+        createpainter()
+        painter.forward(100)
     
-    if season == ("Spring"):
-        print("hi")
-    time.sleep(1)
+    while season == ("Spring"):
+        createpainter()
+        painter.forward(100)
     print(season)
+    tme.sleep(1)
+
+
         
-
-
 
 
 
