d0bb475f922c87d7bc6ebf7a20948753f898b569
Robert Mainhardt <rmainhar@shakopee.k12.mn.us>
2025-09-29 09:41:58 -0500

    Update 119_Mainhardt.py
    Added football shape

--- 1.1.9 - Algorithms and Art/119_Mainhardt.py
+++ 1.1.9 - Algorithms and Art/119_Mainhardt.py
@@ -1,2 +1,18 @@
-# Commit worthy code
-# This code is awesome
+import turtle as trtl 
+
+trtl.addshape("football", (
+    (-50, 0),    # Left end
+    (-20, 25),   # Top-left curve
+    (0, 30),     # Top point
+    (20, 25),    # Top-right curve
+    (50, 0),     # Right end
+    (20, -25),   # Bottom-right curve
+    (0, -30),    # Bottom point
+    (-20, -25)   # Bottom-left curve
+))
+
+fb = trtl.Turtle(shape="football")
+fb.setheading(90)
+
+wn = trtl.Screen()
+wn.mainloop()
