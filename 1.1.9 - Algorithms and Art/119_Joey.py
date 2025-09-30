430ab59207e2c7491187a4f975b12bd4d5e89c58
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-09-30 11:19:36 -0500

    Update 119_Joey.py
    complete redoing of project/abandoning old one for simpler project

--- 1.1.9 - Algorithms and Art/119_Joey.py
+++ 1.1.9 - Algorithms and Art/119_Joey.py
@@ -9,53 +9,52 @@ screen_w = 1500
 wn = trtl.Screen()
 wn.setup(width=screen_w, height=screen_h)
 start = ("idk")
-season = ("idk")
 creating = 1
-def createpainter():
-    painter.pensize(10)
-    painter.pencolor("black")
-
-def changeseason():
-    season = trtl.textinput("Confirmation:","What season would you like? Summer, Fall, Winter, Spring")
-wn.onkeypress(changeseason, "c")
-wn.listen()
-start = trtl.textinput("Confirmation:","Press C to bring up the menu to set the season! Enter 'y' to continue!")
+mtnx = -750
+mtny = 250
+colorlist = [("gray50"),("gray60"),("gray70")]
 
 
-while start != ("y"):
-    start = trtl.textinput("Confirmation:","Press C to bring up the menu to set the season! Enter 'y' to continue!")
+def sky():
+    painter.penup()
+    painter.speed(0)
+    painter.pencolor("DarkSlateGray1")
+    painter.fillcolor("DarkSlateGray1")
+    painter.goto(750,200)
+    painter.setheading(45)
+    painter.pendown()
+    painter.begin_fill()
+    painter.circle(10000, steps = 4)
+    painter.end_fill()
+    painter.penup()
+def mountain():
+    randomcolor = rdm.choice(colorlist)
+    painter.color(randomcolor,randomcolor)
+    painter.begin_fill()
+    painter.setheading(180)
+    randomheight = rdm.randint(100,130)
+    painter.circle(randomheight, steps=3)
+    painter.end_fill()
 
+'trtl.textinput("Confirmation:", "Hello! Are you ready to be sent to a winter wonderland?")'
+sky()
+painter.goto(mtnx,mtny)
+painter.pendown()
+for mountains in range(25):
+    mtny = 250
+    randomy = rdm.randint(-25,25)
+    mtny = mtny + randomy
+    painter.penup()
+    painter.goto(mtnx,mtny)
+    painter.pendown()
+    mountain()
+    painter.penup()
+    painter.setheading(0)
+    randomforward = rdm.randint(50,100)
+    painter.forward(randomforward)
+    mtnx = mtnx + randomforward
+    painter.pendown()
 
-while start == ("y"):
-    changeseason()
-    if season == ("Summer"):
-        if creating == 1:
-            createpainter()
-            painter.forward(100)
-            painter.forward(100)
-            painter.forward(100)
-            painter.forward(100)
-            painter.forward(100)
-            painter.forward(100)
-            creating - 1
-
-    
-    while season == ("Fall"):
-        createpainter()
-        painter.forward(100)
-    
-    while season == ("Winter"):
-        createpainter()
-        painter.forward(100)
-    
-    while season == ("Spring"):
-        createpainter()
-        painter.forward(100)
-    print(season)
-    tme.sleep(1)
-
-
-        
 
 
 
