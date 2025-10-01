c8c4a8260ce33d0c64d87844219f33bdba7c5086
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-10-01 11:20:01 -0500

    Update 119_Joey.py
    complete program basically besides extra features

--- 1.1.9 - Algorithms and Art/119_Joey.py
+++ 1.1.9 - Algorithms and Art/119_Joey.py
@@ -12,7 +12,29 @@ start = ("idk")
 creating = 1
 mtnx = -750
 mtny = 250
+lakecolor = ("lightblue")
 colorlist = [("gray50"),("gray60"),("gray70")]
+trtl.addshape("car",((0,0),(1,0),(1,-3),(4,-3),(4,0),(7,0),(7,-3),(10,-3),(10,0),(11,0),(11,2),(9,2),(9,3),(8,3),(8,4),(5,4),(5,3),(4,3),(4,2),(0,2)))
+carxcor = painter.xcor
+carcolorlist = [
+    "white","black","gray","grey","red","green","blue","cyan","yellow","magenta",
+    "orange","brown","pink","purple","violet","indigo","gold","silver","lime",
+    "maroon","navy","teal","olive","coral","salmon","khaki","plum","orchid",
+    "turquoise","beige","azure","lavender","tan","chocolate","crimson","sienna",
+    "steelblue","slategray","lightgray","darkgray","lightblue","darkblue",
+    "lightgreen","darkgreen","mintcream","seagreen","aquamarine","springgreen",
+    "peru","tomato","fuchsia","papayawhip","lemonchiffon","ivory","linen",
+    "aliceblue","antiquewhite","burlywood","cadetblue","chartreuse","cornflowerblue",
+    "cornsilk","darkorange","darksalmon","deeppink","dodgerblue","firebrick",
+    "gainsboro","hotpink","indianred","lavenderblush","lightcoral","lightcyan",
+    "lightgoldenrodyellow","lightsalmon","lightskyblue","lightsteelblue",
+    "lightyellow","mediumaquamarine","mediumblue","mediumorchid","mediumpurple",
+    "mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise",
+    "mediumvioletred","midnightblue","moccasin","navajowhite","oldlace","palegreen",
+    "paleturquoise","palevioletred","powderblue","rosybrown","royalblue","saddlebrown",
+    "seashell","sienna","skyblue","steelblue","thistle","wheat"
+    ]
+
 
 
 def sky():
@@ -32,11 +54,13 @@ def mountain():
     painter.color(randomcolor,randomcolor)
     painter.begin_fill()
     painter.setheading(180)
-    randomheight = rdm.randint(100,130)
+    randomheight = rdm.randint(130,145)
     painter.circle(randomheight, steps=3)
     painter.end_fill()
 
-'trtl.textinput("Confirmation:", "Hello! Are you ready to be sent to a winter wonderland?")'
+checkno = trtl.textinput("Confirmation:", "Hello! Are you ready to be sent to a winter wonderland?")
+if checkno == ("no","n","NO","N","No","Nah","nah","nope"):
+    trtl.textinput("Too bad.","That is too bad.")
 sky()
 painter.goto(mtnx,mtny)
 painter.pendown()
@@ -54,6 +78,67 @@ for mountains in range(25):
     painter.forward(randomforward)
     mtnx = mtnx + randomforward
     painter.pendown()
+
+
+is_error = False
+while not is_error:
+    lakecolor = trtl.textinput("Color?","What color do you want the lake to be? Recommended is 'lightblue'!")
+    try:
+        painter.color(lakecolor,lakecolor)
+        is_error = True
+    except trtl.TurtleGraphicsError:
+        is_error = False
+
+painter.penup()
+painter.goto (325,-100)
+painter.pendown()
+painter.begin_fill()
+painter.circle(75)
+painter.end_fill()
+painter.penup()
+painter.goto(-325,-100)
+painter.pendown()
+painter.begin_fill()
+painter.circle(75)
+painter.end_fill()
+painter.penup()
+
+painter.goto(750,-350)
+painter.pendown()
+painter.color("LightSlateGray","LightSlateGray")
+painter.setheading(180)
+
+painter.begin_fill()
+painter.forward(1500)
+painter.right(90)
+painter.forward(75)
+painter.right(90)
+painter.forward(1500)
+painter.right(90)
+painter.forward(75)
+painter.right(90)
+painter.end_fill()
+painter.penup()
+
+wn.update()
+
+
+cloop = int(trtl.textinput("Question:","How many cars should go down this highway?"))
+
+painter.tilt(270)
+painter.setheading(180)
+painter.shapesize(10,10,10)
+painter.shape("car")
+
+for car in range(cloop):
+    painter.hideturtle()
+    randomcarcolor = rdm.choice(carcolorlist)
+    painter.color(randomcarcolor,randomcarcolor)
+    painter.speed(0)
+    painter.goto(750,-313)
+    painter.showturtle()
+    painter.speed(3)
+    painter.forward(1600)
 
 
 
