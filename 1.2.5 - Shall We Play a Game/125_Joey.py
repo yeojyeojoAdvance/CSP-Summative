31863016babf4878e1c59d41337f7066672e7461
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-10-28 10:08:14 -0500

    Create 125_Joey.py
    setting up beginning outline for blackjack

---
+++ 1.2.5 - Shall We Play a Game/125_Joey.py
@@ -0,0 +1,5 @@
+# Make deck of cards - 1 hour
+# Make bettable chips - 15 minutes
+# Make the rules of blackjack - 2+ hours
+# Manipulate your chips and record them in a txt file - 1 hour
+# Make the game with a gui - 1 hour
