dcf773f4eb25e331fc6179efb472cdcbd0031fea
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-11-04 09:53:33 -0600

    Update 125_Joey.py
    oct 31st new code

--- 1.2.5 - Shall We Play a Game/125_Joey.py
+++ 1.2.5 - Shall We Play a Game/125_Joey.py
@@ -7,3 +7,15 @@ wn = trtl.Screen()
 wn.setup(width=screen_w, height=screen_h)
 painter = trtl.Turtle()
 painter.goto(0,0)
+
+# Make the game with a decent gui - 1 hour
+
+# Make the gui interactable - 15 minutes
+
+# Make fish randomly sized, with bigger ones being much more rare - 1 hour
+fishsize = rdm.randint(1,100)
+print(fishsize)
+
+# Biggest fish displayed above fisherman - 30 minutes
+
+# Save what your biggest fish is in a save file along with your name to correlate to it - 1 hour
