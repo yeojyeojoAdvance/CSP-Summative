badc36026492043379ef54ac03875efabcb59534
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-11-05 06:04:47 -0600

    checkpt 1
    # Make fish randomly sized, with bigger ones being much more rare

--- 1.2.5 - Shall We Play a Game/125_Joey.py
+++ 1.2.5 - Shall We Play a Game/125_Joey.py
@@ -10,12 +10,27 @@ painter.goto(0,0)
 
 # Make the game with a decent gui - 1 hour
 
+
 # Make the gui interactable - 15 minutes
 
 # Make fish randomly sized, with bigger ones being much more rare - 1 hour
-fishsize = rdm.randint(1,100)
-print(fishsize)
+def getfish():
+    sizefish = "small"
+    fishsize = rdm.randint(0,100)
+    if fishsize >= 50:
+        sizefish = "medium"
+        fishsize = rdm.randint(50,150)
+        if fishsize >= 100:
+            fishsize = rdm.randint(100,200)
+            sizefish = "large"
+            if fishsize >= 150:
+                fishsize = rdm.randint(150,250)
+                sizefish = "xlarge"
+    print(fishsize)
+    print(sizefish)
 
 # Biggest fish displayed above fisherman - 30 minutes
 
 # Save what your biggest fish is in a save file along with your name to correlate to it - 1 hour
+
+getfish()
