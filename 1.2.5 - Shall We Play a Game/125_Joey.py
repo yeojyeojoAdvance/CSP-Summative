1a2a8916e3a61a38cc0df6960396ad9467c8fd71
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-11-05 07:55:24 -0600

    Update 125_Joey.py
    made fish display

--- 1.2.5 - Shall We Play a Game/125_Joey.py
+++ 1.2.5 - Shall We Play a Game/125_Joey.py
@@ -6,27 +6,49 @@ screen_w = 750
 wn = trtl.Screen()
 wn.setup(width=screen_w, height=screen_h)
 painter = trtl.Turtle()
+fish = trtl.Turtle()
+fishcounterscore = trtl.Turtle()
+fishcounterscore2 = trtl.Turtle()
+fish.shape("turtle")
 painter.goto(0,0)
 painter.speed(0)
+
+score = 0
+
+fishcolorlist = [
+    "white","black","gray","grey","red","green","blue","cyan","yellow","magenta",
+    "orange","brown","pink","purple","violet","indigo","gold","silver","lime",
+    "maroon","navy","teal","olive","coral","salmon","khaki","plum","orchid",
+    "turquoise","beige","azure","lavender","tan","chocolate","crimson","sienna",
+    "steelblue","slategray","lightgray","darkgray","darkblue",
+    "lightgreen","darkgreen","mintcream","seagreen","aquamarine","springgreen",
+    "peru","tomato","fuchsia","papayawhip","lemonchiffon","ivory","linen",
+    "aliceblue","antiquewhite","burlywood","cadetblue","chartreuse","cornflowerblue",
+    "cornsilk","darkorange","darksalmon","deeppink","dodgerblue","firebrick",
+    "gainsboro","hotpink","indianred","lavenderblush","lightcoral","lightcyan",
+    "lightgoldenrodyellow","lightsalmon","lightskyblue","lightsteelblue",
+    "lightyellow","mediumaquamarine","mediumblue","mediumorchid","mediumpurple",
+    "mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise",
+    "mediumvioletred","midnightblue","moccasin","navajowhite","oldlace","palegreen",
+    "paleturquoise","palevioletred","powderblue","rosybrown","royalblue","saddlebrown",
+    "seashell","sienna","skyblue","steelblue","thistle","wheat"
+    ]
+
 # Make the game with a decent gui - 1 hour
 painter.color("lightblue")
 painter.begin_fill()
 painter.circle(100)
 painter.end_fill()
 painter.penup()
-painter.setheading(45)
-painter.goto(25,-100)
-painter.color("black")
-painter.pendown()
-painter.circle(radius=50, steps=4)
-painter.setheading(0)
-painter.penup()
 painter.goto(-50,-150)
-painter.write("Click above to fish!", font= ("Papyrus", 25, "normal"))
+painter.color("black")
+painter.write("Click to fish!", font= ("Papyrus", 25, "normal"))
 # Make the gui interactable - 15 minutes
 
 # Make fish randomly sized, with bigger ones being much more rare - 1 hour
 def getfish():
+    global fishsize
+    global sizefish 
     sizefish = "small"
     fishsize = rdm.randint(0,100)
     if fishsize >= 50:
@@ -42,11 +64,37 @@ def getfish():
     print(sizefish)
 
 # Biggest fish displayed above fisherman - 30 minutes
+def fishtrtl():
+    fish.penup()
+    fish.goto(0,75)
+    fish.speed(1)
+    fish.color(rdm.choice(fishcolorlist))
+    if sizefish == "small":
+        fish.shapesize(1)
+    elif sizefish == "medium":
+        fish.shapesize(3)
+    elif sizefish == "large":
+        fish.shapesize(5)
+    elif sizefish == "xlarge":
+        fish.shapesize(7)
+    fish.goto(0,250)
+    showfish()
 
+def showfish():
+    fishcounterscore.penup()
+    fishcounterscore2.penup()
+    fishcounterscore.speed(0)
+    fishcounterscore2.speed(0)
+    fishcounterscore.hideturtle()
+    fishcounterscore2.hideturtle()
+    fishcounterscore.goto(-175,250)
+    fishcounterscore2.goto(-175,225)
+    fishcounterscore.write("The fish is " + sizefish, font= ("Papyrus", 20, "normal"))
+    fishcounterscore2.write(fishsize, font= ("Papyrus", 20, "normal"))
 # Save what your biggest fish is in a save file along with your name to correlate to it - 1 hour
 
 getfish()
-
+fishtrtl()
 
 
 
