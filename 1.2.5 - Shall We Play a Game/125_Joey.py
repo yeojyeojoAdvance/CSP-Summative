b6c83d188d60ff571b8350472bdfee1f6c45e3c3
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-11-05 06:20:25 -0600

    checkpt 2
    # Make the game with a decent gui

--- 1.2.5 - Shall We Play a Game/125_Joey.py
+++ 1.2.5 - Shall We Play a Game/125_Joey.py
@@ -1,16 +1,28 @@
 import turtle as trtl
 import random as rdm
 
-screen_h = 1000
-screen_w = 1500
+screen_h = 750
+screen_w = 750
 wn = trtl.Screen()
 wn.setup(width=screen_w, height=screen_h)
 painter = trtl.Turtle()
 painter.goto(0,0)
-
+painter.speed(0)
 # Make the game with a decent gui - 1 hour
-
-
+painter.color("lightblue")
+painter.begin_fill()
+painter.circle(100)
+painter.end_fill()
+painter.penup()
+painter.setheading(45)
+painter.goto(25,-100)
+painter.color("black")
+painter.pendown()
+painter.circle(radius=50, steps=4)
+painter.setheading(0)
+painter.penup()
+painter.goto(-50,-150)
+painter.write("Click above to fish!", font= ("Papyrus", 25, "normal"))
 # Make the gui interactable - 15 minutes
 
 # Make fish randomly sized, with bigger ones being much more rare - 1 hour
@@ -34,3 +46,8 @@ def getfish():
 # Save what your biggest fish is in a save file along with your name to correlate to it - 1 hour
 
 getfish()
+
+
+
+
+wn.mainloop()
