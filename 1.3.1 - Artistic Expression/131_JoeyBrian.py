6c775811856bd7d73948323302b9fe470c5b9c80
yeojyeojoAdvance <91613999+yeojyeojoAdvance@users.noreply.github.com>
2025-11-11 11:13:33 -0600

    Envelope that opens to a map

--- 1.3.1 - Artistic Expression/131_JoeyBrian.py
+++ 1.3.1 - Artistic Expression/131_JoeyBrian.py
@@ -1,2 +1,39 @@
 import turtle as trtl
-import playsound as ply
+import random as rand
+from playsound import playsound
+wn = trtl.Screen()
+drawer = trtl.Turtle()
+wn.screensize(600,700)
+
+#Envelope that opens on click 
+open_envelope="openenvelope.gif"
+closed_envelope="closedenvelope.gif"
+map="map.gif"
+wn.addshape(open_envelope)
+wn.addshape(closed_envelope)
+wn.bgpic(closed_envelope)
+def envelopeopener(x,y):
+    wn.bgpic(open_envelope)
+    trtl.onscreenclick(mapopener)
+
+def mapopener(x,y):
+    wn.bgpic(map)
+
+trtl.onscreenclick(envelopeopener)
+
+
+
+
+#Make interactable sections of the map
+
+#Golf
+
+#Shuffleboard
+
+#Music?
+
+#Exit button
+
+
+
+wn.mainloop()
